d270a713d4dc3024e1531e12a2735d1f8a7e1ede
Adam Clemons <adamlouisclemons@gmail.com>
2021-03-05 15:09:52 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,16 +3,23 @@ require 'nokogiri'
 require 'pry'
 
 
-def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
-
-  kickstarter = Nokogiri::HTML(html)
-  # binding.pry
-end
-
-create_project_hash
-
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+
+def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+
+  projects = {}
+
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+
+  projects  
+  # binding.pry
+end
+
+create_project_hash
