de42de3a0bb20602b67a74899a8d93c7917cd4a5
Adam Clemons <adamlouisclemons@gmail.com>
2021-03-05 15:32:30 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -20,12 +20,13 @@ def create_project_hash
     projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i,
-      :description => project.css("p.bbcard_blurb").text
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("span.location-name")
     }
   end
 
   projects
-  binding.pry
+  # binding.pry
 end
 
 create_project_hash
