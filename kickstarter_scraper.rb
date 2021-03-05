244b5e37d62a3cc2b3442be4fac919fbbb8646f4
Adam Clemons <adamlouisclemons@gmail.com>
2021-03-05 15:03:20 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,7 +7,11 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  # binding.pry
 end
 
 create_project_hash
+
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
