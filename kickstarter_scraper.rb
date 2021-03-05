48a0b9a3c9a4294636fabab3dbc009f8a6431f59
Adam Clemons <adamlouisclemons@gmail.com>
2021-03-05 15:06:57 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,3 +15,4 @@ create_project_hash
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
+# funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
