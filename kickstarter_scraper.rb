18abd983c7af8c6aefc18fc4fc750c20ed98b3dd
Adam Clemons <adamlouisclemons@gmail.com>
2021-03-05 15:11:53 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,11 +14,11 @@ def create_project_hash
 
   projects = {}
 
-  kickstarter.css("li.project.grid_4").each do |project|
+  kickstarter.css("").each do |project|
     projects[project] = {}
   end
 
-  projects  
+  projects
   # binding.pry
 end
 
